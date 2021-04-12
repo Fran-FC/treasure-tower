1bcca7647ccca53c48f4f491757e50f88f4de393
Andres Roca Clemente <anrocle@inf.upv.es>
2021-04-12 16:44:57 +0200

    -

--- Treasure Tower/Assets/Scripts/Enemy.cs
+++ Treasure Tower/Assets/Scripts/Enemy.cs
@@ -1,20 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Enemy : MonoBehaviour
 {
+    private Vector3 siguienteMovimiento;
+    public bool armor;
+    public float moveSpeed;
+    public float orientation = 1f;
+    private bool inRange = false;
+    public Player player;
+
     // Start is called before the first frame update
-    private void Awake() {
+    void Start()
+    {
+        //player = GetComponent<Player>();
+
+    }
+    private void Awake()
+    {
         Messenger.AddListener(GameEvent.MOVE_ORDER, OnMyTurn);
     }
-    private void OnDestroy() {
+    private void OnDestroy()
+    {
         Messenger.RemoveListener(GameEvent.MOVE_ORDER, OnMyTurn);
-        
+
     }
 
-    private void OnMyTurn() {
-        // ToDo move enemy
-        transform.position += new Vector3(1, 0, 0);
+    private void OnMyTurn()
+    {
+        CalcInRange();
+        if (inRange)
+        {
+            Attack();
+        } else
+        {
+            CalcPath();
+            this.transform.position += siguienteMovimiento;
+            Debug.Log("Transform position: " + this.transform.position);
+        }
+        //TD
+
+    }
+
+    private void CalcInRange()
+    {
+        if(player.transform.position.x == this.transform.position.x && (player.transform.position.y == this.transform.position.y - 1 || player.transform.position.y == this.transform.position.y + 1))
+        {
+            inRange = true;
+        } else 
+            if(player.transform.position.y == this.transform.position.y && (player.transform.position.x == this.transform.position.x - 1 || player.transform.position.x == this.transform.position.x + 1)) {
+            inRange = true;
+                } 
+        else
+        {
+            inRange = false;
+        }
+    }
+    private void Attack()
+    {
+        //TD
+    }
+    private void CalcPath()
+    {
+        Vector3 aux = new Vector3(this.transform.position.x - player.transform.position.x, this.transform.position.y - player.transform.position.y, 0);
+        if (Math.Abs(aux.x) > Math.Abs(aux.y))
+        {
+            float nuevaX = Math.Sign(aux.x) * -1;
+            siguienteMovimiento = new Vector3(nuevaX, 0f, 0f);
+        }
+        else
+        {
+            float nuevaY = Math.Sign(aux.y) * -1;
+            siguienteMovimiento = new Vector3(0f, nuevaY, 0f);
+        }
     }
 }
---
+++ Treasure Tower/Assets/Scripts/GridBehaviorTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehaviorTest : MonoBehaviour
+{
+    public int rows;
+    public int columns;
+    public int scale = 1;
+    public GameObject gridPrefab;
+    public Vector3 leftBottonLocation = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void GenerateGrid()
+    {
+        for (int i = 0; i < columns; i++)
+        {
+            for(int j=0; j < rows; j++)
+            {
+                GameObject obj = Instantiate(gridPrefab, new Vector3(leftBottonLocation.x + scale*i ,leftBottonLocation.y + scale*j,0), Quaternion.identity);
+            }
+        }
+    }
+}
