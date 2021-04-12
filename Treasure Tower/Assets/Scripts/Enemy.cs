15ebd240b5e1e53b58c722610d3b3b2953aec856
Andres Roca Clemente <anrocle@inf.upv.es>
2021-04-12 16:45:25 +0200

    Script enemy sin terminar

--- Treasure Tower/Assets/Scripts/Enemy.cs
+++ Treasure Tower/Assets/Scripts/Enemy.cs
@@ -5,18 +5,43 @@ using System;
 
 public class Enemy : MonoBehaviour
 {
+    private TileMapGenerator mapGridInfo;
     private Vector3 siguienteMovimiento;
     public bool armor;
     public float moveSpeed;
-    public float orientation = 1f;
+    public Vector2 orientation = new Vector2(0,0);
     private bool inRange = false;
-    public Player player;
+    [SerializeField]
+    private Player player;
 
     // Start is called before the first frame update
+    /*enum WhichEnemy
+    {
+        skeleton = 0,
+        ghost = 1,
+        troll = 2,
+        fire = 3
+    }*/
+    int whichEnemy;
+    SpriteRenderer spriteRenderer;
+
     void Start()
     {
         //player = GetComponent<Player>();
+        mapGridInfo = (TileMapGenerator)FindObjectOfType(typeof(TileMapGenerator));
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        whichEnemy = new System.Random().Next(0, 3);
+        InitSprite();
+    }
 
+    void InitSprite()
+    {
+        switch (whichEnemy)
+        {
+            case 0:
+
+                break;
+        }
     }
     private void Awake()
     {
@@ -36,14 +61,21 @@ public class Enemy : MonoBehaviour
             Attack();
         } else
         {
-            CalcPath();
-            this.transform.position += siguienteMovimiento;
-            Debug.Log("Transform position: " + this.transform.position);
+            if (CanWalk())
+            {
+                siguienteMovimiento = CalcPath();
+                this.transform.position += siguienteMovimiento;
+                ChangeAnimWalk();
+            }
         }
         //TD
 
     }
 
+    private void ChangeAnimWalk()
+    {
+
+    }
     private void CalcInRange()
     {
         if(player.transform.position.x == this.transform.position.x && (player.transform.position.y == this.transform.position.y - 1 || player.transform.position.y == this.transform.position.y + 1))
@@ -62,18 +94,29 @@ public class Enemy : MonoBehaviour
     {
         //TD
     }
-    private void CalcPath()
+    private Vector3 CalcPath()
     {
+        orientation = new Vector2(0, 0);
         Vector3 aux = new Vector3(this.transform.position.x - player.transform.position.x, this.transform.position.y - player.transform.position.y, 0);
-        if (Math.Abs(aux.x) > Math.Abs(aux.y))
+        float nuevaX = Math.Sign(aux.x) * -1;
+        if (mapGridInfo.isTileWalkable(this.transform.position.x + nuevaX, 0))
         {
-            float nuevaX = Math.Sign(aux.x) * -1;
-            siguienteMovimiento = new Vector3(nuevaX, 0f, 0f);
+            orientation.x = nuevaX;
+            return new Vector3(nuevaX, 0f, 0f);
         }
-        else
+
+        float nuevaY = Math.Sign(aux.y) * -1;
+        orientation.y = nuevaY;
+        return new Vector3(0f, nuevaY, 0f);
+        
+    }
+   private bool CanWalk()
+    {
+        bool canWalk = false;
+        if(mapGridInfo.isTileWalkable(this.transform.position.x-1,0) || mapGridInfo.isTileWalkable(this.transform.position.x + 1, 0) || mapGridInfo.isTileWalkable(0, this.transform.position.y - 1) || mapGridInfo.isTileWalkable(0, this.transform.position.y - 1))
         {
-            float nuevaY = Math.Sign(aux.y) * -1;
-            siguienteMovimiento = new Vector3(0f, nuevaY, 0f);
+            canWalk = true;
         }
+        return canWalk;
     }
 }
