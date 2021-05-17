423bb0eeb0c3e82f8e5ee00d82c08cffa1e3f1d2
Francesc Folch Company <frafolcm@gmail.com>
2021-05-17 21:08:09 +0200

    gameover screen v2

--- Treasure Tower/Assets/Scripts/Controllers/GameOverController.cs
+++ Treasure Tower/Assets/Scripts/Controllers/GameOverController.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameOverController : MonoBehaviour
 {
     public void Setup() {
         gameObject.SetActive(true);
+    }
+
+    public void RestartButton(){
+        SceneManager.LoadScene("ProcMapGen");
+    }
+
+    public void ExitButton() {
+        SceneManager.LoadScene("Menu");
     }
 }
